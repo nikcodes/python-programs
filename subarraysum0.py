b8bd906601377f080026a21e6de62196feea6f3b
Nikhil Rathore <35673596+nikcodes@users.noreply.github.com>
2018-03-23 18:54:59 +0530

    Create subarraysum0.py
    check if there is a subarray having sum equal to 0

---
+++ subarraysum0.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    n=int(input())
+    a=[int(i) for i in input().split()]
+    b=[a[0]]
+    for e in a[1:]:
+        b.append(b[-1]+e)
+    if len(set(b))<n:
+        print('Yes')
+    else:
+        print('No')
