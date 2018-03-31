8fc2fa979556e9d05897880ac549ec59a008115d
Nikhil Rathore <35673596+nikcodes@users.noreply.github.com>
2018-03-31 15:19:11 +0530

    Update notpalindrome.py

--- notpalindrome.py
+++ notpalindrome.py
@@ -1,18 +1,9 @@
 s=input()
 a=s[::-1]
 n=len(s)
-if s!=a:
+if len(set(list(s)))==1:
+    print(0)
+elif s!=a:
     print(n)
 else:
-    c=0;m=0
-    for e in s:
-        if e!=s[-1]:
-            break
-        c+=1
-    m=max(m,n-c)
-    c=0
-    for e in s[::-1]:
-        if e!=s[0]:
-            break
-        c+=1
-    print(max(m,n-c))
+    print(n-1)
